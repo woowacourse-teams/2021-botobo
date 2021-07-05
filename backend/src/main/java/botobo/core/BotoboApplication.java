6a2d21dd35af0aa4a8de26e11dc9a50a65199f48
pkeugine <pkeugine@gmail.com>
2021-07-05 17:27:03 +0900

    etc: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/botobo/core/BotoboApplication.java
@@ -0,0 +1,13 @@
+package botobo.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotoboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotoboApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/botobo/core/BotoboApplicationTests.java
@@ -0,0 +1,13 @@
+package botobo.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotoboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
