e70066c8043cddba5bce82bb39d03d9b0da4e7db
seovalue <seominjeong.dev@gmail.com>
2021-07-07 18:51:22 +0900

    etc: 불필요한 메서드 삭제

--- backend/src/main/java/botobo/core/card/domain/AnswerRepository.java
+++ backend/src/main/java/botobo/core/card/domain/AnswerRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-
-    Optional<Answer> findByContent(String content);
 }
