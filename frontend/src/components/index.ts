136f5d17cbe19b1d39024e1aa88347078de01687
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-07-10 00:18:00 +0900

    UI03 - 퀴즈 페이지 UI 구현  (#40)
    * etc: emotion.d.ts 파일 분리
    
    * feat: QnACard 추가
    
    * feat: Quiz 페이지 구현 완료

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as QuizStarter } from './QuizStarter';
 export { default as Category } from './Category';
 export { default as CategoryList } from './CategoryList';
 export { default as Button } from './Button';
+export { default as QnACard } from './QnACard';
---
+++ frontend/src/pages/QuizPage.tsx
@@ -0,0 +1,78 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { QnACard } from '../components';
+import { Flex } from '../styles';
+
+const QuizPage = () => (
+  <Container>
+    <Tooltip>
+      <TooltipDescription>
+        카드를 클릭해 질문과 정답을 확인할 수 있어요.
+      </TooltipDescription>
+    </Tooltip>
+    <QnACard
+      question="리버스 프록시란 무엇인가요?"
+      answer="리버스 프록시란 어쩌고 저쩌고 입니다."
+      categoryName="Network"
+    />
+    <PageNation>
+      <button>
+        <i className="fas fa-arrow-left"></i>
+      </button>
+      <Page>1 / 10</Page>
+      <button>
+        <i className="fas fa-arrow-right"></i>
+      </button>
+    </PageNation>
+  </Container>
+);
+
+const Container = styled.div`
+  ${Flex({ justify: 'center', items: 'center', direction: 'column' })};
+  height: 100%;
+  width: 100%;
+`;
+
+const Tooltip = styled.div`
+  ${Flex({ justify: 'center', items: 'center' })};
+  width: 100%;
+  height: 2.5rem;
+  margin-bottom: 0.5rem;
+  ${({ theme }) => css`
+    background-color: ${theme.color.indigo};
+    border-radius: ${theme.borderRadius.square};
+  `}
+`;
+
+const TooltipDescription = styled.span`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.small};
+    color: ${theme.color.white};
+  `}
+`;
+
+const PageNation = styled.div`
+  ${Flex({ items: 'center' })};
+  margin-top: 5rem;
+
+  & > button {
+    ${({ theme }) =>
+      css`
+        font-size: ${theme.fontSize.medium};
+      `}
+  }
+
+  svg {
+    height: inherit;
+  }
+`;
+
+// TODO: margin-bottom 안쓰고 맞추기
+const Page = styled.span`
+  margin: 0 1.5rem;
+  margin-bottom: 3px;
+`;
+
+export default QuizPage;
