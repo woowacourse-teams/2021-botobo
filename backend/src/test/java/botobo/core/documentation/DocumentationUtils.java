e9dff9afea75d37f14677401efa6ba0eb19259b5
seunghyun <ggyool@naver.com>
2021-07-11 16:40:33 +0900

    test: 카테고리 조회 문서화 테스트 추가

---
+++ backend/src/test/java/botobo/core/documentation/DocumentationUtils.java
@@ -0,0 +1,19 @@
+package botobo.core.documentation;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+public class DocumentationUtils {
+
+    public static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    public static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
