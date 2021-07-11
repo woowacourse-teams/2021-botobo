af2d6bf211fd7f22093fe6db7f71d79cad1b2b39
ohjoohyung <xntm3324@naver.com>
2021-07-11 17:53:49 +0900

    feat: cors 설정 완료 (#55)

---
+++ backend/src/main/java/botobo/core/config/WebConfig.java
@@ -0,0 +1,16 @@
+package botobo.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .maxAge(3000);
+    }
+}
