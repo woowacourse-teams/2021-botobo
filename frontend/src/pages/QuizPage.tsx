98a7870fc1dbc632a4b8772591a2a02ebb42d24e
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-07-11 14:46:59 +0900

    feat: 퀴즈 페이지 슬라이드 구현 (#51)

--- frontend/src/pages/QuizPage.tsx
+++ frontend/src/pages/QuizPage.tsx
@@ -8,6 +8,18 @@ import { Quiz } from '../components';
 import { ROUTE } from '../constants';
 import { Flex } from '../styles';
 
+interface TooltipProps {
+  isTooltipVisible: boolean;
+}
+interface QuizListProps {
+  quizCount: number;
+  currentIndex: number;
+}
+
+interface QuizItemProps {
+  quizIndex: number;
+}
+
 const quizzes = [
   {
     id: 1,
@@ -48,50 +60,56 @@ const quizzes = [
 ];
 
 const QuizPage = () => {
-  const [quizIndex, setQuizIndex] = useState(0);
+  const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
+  const [prevQuizId, setPrevQuizId] = useState<number | null>(null);
+
   const history = useHistory();
 
-  const { id, question, answer, categoryName } = quizzes[quizIndex];
-
   const showPrevQuiz = () => {
-    if (quizIndex === 0) return;
+    if (currentQuizIndex === 0) return;
 
-    setQuizIndex((prevValue) => prevValue - 1);
+    setPrevQuizId(quizzes[currentQuizIndex].id);
+    setCurrentQuizIndex((prevValue) => prevValue - 1);
   };
 
   const showNextQuiz = () => {
-    if (quizIndex === quizzes.length - 1) {
+    if (currentQuizIndex === quizzes.length - 1) {
       history.push(ROUTE.QUIZ_RESULT);
 
       return;
     }
 
-    setQuizIndex((prevValue) => prevValue + 1);
+    setPrevQuizId(quizzes[currentQuizIndex].id);
+    setCurrentQuizIndex((prevValue) => prevValue + 1);
   };
 
   return (
     <Container>
       <QuizWrapper>
-        {quizIndex === 0 && (
-          <Tooltip>
-            <TooltipDescription>
-              카드를 클릭해 질문과 정답을 확인할 수 있어요.
-            </TooltipDescription>
-          </Tooltip>
+        <Tooltip isTooltipVisible={currentQuizIndex === 0}>
+          카드를 클릭해 질문과 정답을 확인할 수 있어요.
+        </Tooltip>
+        {quizzes && (
+          <QuizList quizCount={quizzes.length} currentIndex={currentQuizIndex}>
+            {quizzes.map(({ id, question, answer, categoryName }, index) => (
+              <QuizItem key={id} quizIndex={index}>
+                <Quiz
+                  question={question}
+                  answer={answer}
+                  categoryName={categoryName}
+                  isChanged={id === prevQuizId}
+                />
+              </QuizItem>
+            ))}
+          </QuizList>
         )}
-        <Quiz
-          id={id}
-          question={question}
-          answer={answer}
-          categoryName={categoryName}
-        />
       </QuizWrapper>
       <PageNation>
         <button onClick={showPrevQuiz}>
           <i className="fas fa-arrow-left"></i>
         </button>
         <Page>
-          {quizIndex + 1} / {quizzes.length}
+          {currentQuizIndex + 1} / {quizzes.length}
         </Page>
         <button onClick={showNextQuiz}>
           <i className="fas fa-arrow-right"></i>
@@ -104,6 +122,7 @@ const QuizPage = () => {
 const Container = styled.div`
   ${Flex({ justify: 'center', items: 'center', direction: 'column' })};
   width: 100%;
+  overflow: hidden;
 
   ${({ theme }) =>
     css`
@@ -117,24 +136,44 @@ const QuizWrapper = styled.div`
   width: 100%;
 `;
 
-const Tooltip = styled.div`
+const Tooltip = styled.div<TooltipProps>`
   ${Flex({ justify: 'center', items: 'center' })};
   position: absolute;
   width: 100%;
   height: 2.5rem;
   top: -3rem;
   margin-bottom: 0.5rem;
-  ${({ theme }) => css`
+  transition: opacity 0.1s ease;
+
+  ${({ theme, isTooltipVisible }) => css`
     background-color: ${theme.color.indigo};
     border-radius: ${theme.borderRadius.square};
-  `}
-`;
-
-const TooltipDescription = styled.span`
-  ${({ theme }) => css`
+    opacity: ${isTooltipVisible ? 1 : 0};
     font-size: ${theme.fontSize.small};
     color: ${theme.color.white};
   `}
+`;
+
+const QuizList = styled.ul<QuizListProps>`
+  ${Flex()};
+  transition: transform 0.15s ease;
+
+  ${({ quizCount, currentIndex }) => css`
+    width: calc(100% * ${quizCount});
+    transform: translateX(
+      calc(-${(100 / quizCount) * currentIndex}% - ${1.25 * currentIndex}rem)
+    );
+
+    & > li {
+      width: calc(100% / ${quizCount});
+    }
+  `};
+`;
+
+const QuizItem = styled.li<QuizItemProps>`
+  ${({ quizIndex }) => css`
+    transform: translateX(${1.25 * quizIndex}rem);
+  `};
 `;
 
 const PageNation = styled.div`
@@ -154,10 +193,8 @@ const PageNation = styled.div`
   }
 `;
 
-// TODO: margin-bottom 안쓰고 맞추기
 const Page = styled.span`
   margin: 0 1.5rem;
-  margin-bottom: 3px;
 `;
 
 export default QuizPage;
