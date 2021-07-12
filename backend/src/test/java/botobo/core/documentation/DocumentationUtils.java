641d658a3b2322294d5da7f022689d35c3c503aa
seovalue <seominjeong.dev@gmail.com>
2021-07-12 18:57:03 +0900

    etc: 코드 정리

--- backend/src/test/java/botobo/core/documentation/DocumentationUtils.java
+++ backend/src/test/java/botobo/core/documentation/DocumentationUtils.java
@@ -3,9 +3,7 @@ package botobo.core.documentation;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 
 public class DocumentationUtils {
 
