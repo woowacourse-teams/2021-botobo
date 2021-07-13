48febbe087234d9f38e389e3e3c492f34065e1db
Eugine Park <pkeugine@gmail.com>
2021-07-13 14:23:07 +0900

    etc: admin 문서 테스트 import문 추가

--- backend/src/test/java/botobo/core/documentation/AdminDocumentationTest.java
+++ backend/src/test/java/botobo/core/documentation/AdminDocumentationTest.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
