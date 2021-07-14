6fe764df857afad5b8b09b0191fe8c27cd24b809
pkeugine <pkeugine@gmail.com>
2021-07-14 12:05:45 +0900

    feat: Category 불러올 때 Card fetch join 하도록 변경

--- backend/src/main/java/botobo/core/quiz/domain/category/CategoryRepository.java
+++ backend/src/main/java/botobo/core/quiz/domain/category/CategoryRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("select c from Category c join fetch c.cards where c.id = :id")
+    @Query("select c from Category c join fetch c.cards.cards where c.id = :id")
     Optional<Category> findCategoryAndCardsByIdJoinFetch(@Param("id") Long id);
 }
