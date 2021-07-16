93c0abd2aa87d75153d2ae74d03bfa7adc405907
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-07-16 21:20:41 +0900

    fix: publicPath 추가
    Co-authored-by: Kyle <mkitigy@gmail.com>

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -6,8 +6,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './public/index.html' }),
