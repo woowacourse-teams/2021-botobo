3d3559a44548b79ba44fa97f3cfce189bb1dc8e8
seunghyun <ggyool@naver.com>
2021-07-22 02:56:17 +0900

    etc: 데이터베이스에서 초기 데이터로 사용할 쿼리 생성

---
+++ backend/src/docs/sql/ddl.sql
@@ -0,0 +1,22 @@
+CREATE TABLE workbook (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    is_public TINYINT(1) NOT NULL,
+    is_deleted TINYINT(1) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE card (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    question BLOB NOT NULL,
+    answer BLOB NOT NULL,
+    encounter_count INT NOT NULL,
+    is_bookmark TINYINT(1) not null,
+    is_next_quiz TINYINT(1) not null,
+    workbook_id BIGINT not null,
+    is_deleted TINYINT(1) not null,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(workbook_id) REFERENCES workbook(id) ON UPDATE CASCADE ON DELETE RESTRICT
+)
