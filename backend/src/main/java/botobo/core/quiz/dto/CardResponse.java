793ac579d9ee1763201287b47833067e8fbf5aed
seunghyun <ggyool@naver.com>
2021-07-22 19:44:32 +0900

    feat: CardResponse에 필드 추가

--- backend/src/main/java/botobo/core/quiz/dto/CardResponse.java
+++ backend/src/main/java/botobo/core/quiz/dto/CardResponse.java
@@ -18,12 +18,20 @@ public class CardResponse {
     private Long id;
     private String question;
     private String answer;
+    private Long workbookId;
+    private boolean isBookmark;
+    private boolean isNextQuiz;
+    private int encounterCount;
 
     public static CardResponse of(Card card) {
         return CardResponse.builder()
                 .id(card.getId())
                 .question(card.getQuestion())
                 .answer(card.getAnswer())
+                .workbookId(card.getWorkbook().getId())
+                .isBookmark(card.isBookmark())
+                .isNextQuiz(card.isNextQuiz())
+                .encounterCount(card.getEncounterCount())
                 .build();
     }
 
