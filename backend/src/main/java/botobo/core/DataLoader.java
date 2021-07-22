8592722bc7029f779199c9304139d6c2f59e0c88
ggyool <ggyool@naver.com>
2021-07-22 15:41:18 +0900

    B28 - 로컬에서 실행 시 필요한 초기데이터를 위한 데이터로더 리팩토링 (#176)
    * etc: application.yml 수정
    
    * refactor: 데이터로더 리팩터링
    
    * etc: 데이터베이스에서 초기 데이터로 사용할 쿼리 생성

--- backend/src/main/java/botobo/core/DataLoader.java
+++ backend/src/main/java/botobo/core/DataLoader.java
@@ -1,94 +1,103 @@
 package botobo.core;
 
-//@Component
-//@Slf4j
-//@Profile("!test")
-public class DataLoader {
-//
-//    @Value("${dummy.file-path}")
-//    private String filePath;
-//    @Value("${dummy.categories}")
-//    private List<String> categories;
-//    private WorkbookRepository workbookRepository;
-//    private CardRepository cardRepository;
+import botobo.core.quiz.domain.card.Card;
+import botobo.core.quiz.domain.card.CardRepository;
+import botobo.core.quiz.domain.workbook.Workbook;
+import botobo.core.quiz.domain.workbook.WorkbookRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-//    public DataLoader(WorkbookRepository workbookRepository, CardRepository cardRepository) {
-//        this.workbookRepository = workbookRepository;
-//        this.cardRepository = cardRepository;
-//    }
-//
-//    @Override
-//    public void run(String... args) {
-//        if (!workbookRepository.findAll().isEmpty()) {
-//            return;
-//        }
-//        for (String category : categories) {
-//            readFile(filePath + category);
-//        }
-//    }
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
-//    public void readFile(String filePath) {
-//        try {
-//            File file = new File(filePath);
-//            FileReader fileReader = new FileReader(file);
-//
-//            BufferedReader bufferedReader = new BufferedReader(fileReader);
-//            setData(bufferedReader);
-//            bufferedReader.close();
-//        } catch (FileNotFoundException e) {
-//            log.info("Dummy Data 파일 경로를 찾지 못했습니다.");
-//        } catch (IOException e) {
-//            log.info("Dummy Data를 불러오는 중 자바 입출력 에러가 발생했습니다.");
-//        }
-//    }
+@Component
+@Slf4j
+@Profile("local")
+public class DataLoader implements CommandLineRunner {
 
-//    private void setData(BufferedReader bufferedReader) throws IOException {
-//        String line = "";
-//        while ((line = bufferedReader.readLine()) != null) {
-//            Workbook workbook = makeWorkbook(line);
-//            Card card = Card.builder()
-//                    .workbook(workbook)
-//                    .question("")
-//                    .isDeleted(false)
-//                    .build();
-//            setQuestionAndAnswer(bufferedReader, workbook);
-//        }
-//    }
+    @Value("${dummy.file-path}")
+    private String filePath;
 
-//    private void setQuestionAndAnswer(BufferedReader bufferedReader, Workbook workbook) throws IOException {
-//        String line = "";
-//        while ((line = bufferedReader.readLine()) != null) {
-//            if (count % 2 == 0) {
-//                card = setQuestion(workbook, line);
-//            } else {
-//                setAnswer(card, line);
-//            }
-//
-//        }
-//    }
-//
-//    private Workbook makeWorkbook(String workbookName) {
-//        Workbook workbook = Workbook.builder()
-//                .name(workbookName)
-//                .isDeleted(false).build();
-//        return workbookRepository.save(workbook);
-//    }
+    @Value("${dummy.bootrun-file-path}")
+    private String bootrunFilePath;
 
-//    private Card setQuestion(Workbook workbook, String question) {
-//        Card card = Card.builder()
-//                .question(question)
-//                .answer(answer)
-//                .workbook(workbook)
-//                .isDeleted(false)
-//                .build();
-//        return cardRepository.save(card);
-//    }
+    @Value("${dummy.workbooks}")
+    private List<String> workbooks;
 
-//    private void setAnswer(Card card, String answer) {
-//        answerRepository.save(Answer.builder()
-//                .content(answer)
-//                .card(card)
-//                .isDeleted(false)
-//                .build());
-//    }
+    private final WorkbookRepository workbookRepository;
+    private final CardRepository cardRepository;
+
+    public DataLoader(WorkbookRepository workbookRepository, CardRepository cardRepository) {
+        this.workbookRepository = workbookRepository;
+        this.cardRepository = cardRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        String targetPath = filePath;
+        if (isBootrun(bootrunFilePath)) {
+            targetPath = bootrunFilePath;
+        }
+        for (String workbook : workbooks) {
+            readFile(targetPath + workbook);
+        }
+    }
+
+    private boolean isBootrun(String filePath) {
+        return new File(filePath).exists();
+    }
+
+    public void readFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            saveData(bufferedReader);
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            log.info("Dummy Data 파일 경로를 찾지 못했습니다.");
+        } catch (IOException e) {
+            log.info("Dummy Data를 불러오는 중 자바 입출력 에러가 발생했습니다.");
+        }
+    }
+
+    private void saveData(BufferedReader bufferedReader) throws IOException {
+        String workbookName = bufferedReader.readLine();
+        if (Objects.isNull(workbookName)) {
+            return;
+        }
+        Workbook workbook = saveWorkbook(workbookName);
+        String question = bufferedReader.readLine();
+        String answer = bufferedReader.readLine();
+
+        while (Objects.nonNull(question) && Objects.nonNull(answer)) {
+            saveCard(workbook, question, answer);
+            question = bufferedReader.readLine();
+            answer = bufferedReader.readLine();
+        }
+    }
+
+    private Workbook saveWorkbook(String workbookName) {
+        Workbook workbook = Workbook.builder()
+                .name(workbookName)
+                .build();
+        return workbookRepository.save(workbook);
+    }
+
+    private void saveCard(Workbook workbook, String question, String answer) {
+        Card card = Card.builder()
+                .workbook(workbook)
+                .question(question)
+                .answer(answer)
+                .build();
+        cardRepository.save(card);
+    }
 }
