74466c96769f44e9393f90f214498d24feeee297
Eugine Park <pkeugine@gmail.com>
2021-07-22 14:53:43 +0900

    B14 - 로깅 전략 wiki 문서화 및 xml 코드 상 반영 (#172)
    * etc: 서브모듈 변경사항 적용
    
    * feat: http관련 로깅 설정
    
    * feat: 기존 로그 패턴 수정 및 저장 레벨 변경
    
    * feat: 로그 레벨 별 파일 분리
    
    * etc: 로깅 관련 설정 변경
    
    * feat: 파일, 콘솔 로깅 어펜더 추가
    
    * feat: logback 파일에 분리된 어펜더 추가
    
    * feat: http 요청, 응답 body 출력 관련 설정 추가
    
    Co-authored-by: seunghyun <ggyool@naver.com>

--- backend/src/main/java/botobo/core/config/WebConfig.java
+++ backend/src/main/java/botobo/core/config/WebConfig.java
@@ -1,8 +1,12 @@
 package botobo.core.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .maxAge(3000);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
+                jacksonConverter.setPrettyPrint( true );
+            }
+        }
     }
 }
