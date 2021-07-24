30798b3edce347f6e4caec7c6b76ced6c372afd1
pkeugine <pkeugine@gmail.com>
2021-07-24 11:19:59 +0900

    test: 카드 생성 관련 인수 테스트와 서비스 테스트 추가

--- backend/src/test/java/botobo/core/quiz/CardAcceptanceTest.java
+++ backend/src/test/java/botobo/core/quiz/CardAcceptanceTest.java
@@ -1,7 +1,9 @@
 package botobo.core.quiz;
 
 import botobo.core.DomainAcceptanceTest;
-import botobo.core.auth.AuthAcceptanceTest;
+import botobo.core.common.exception.ErrorResponse;
+import botobo.core.quiz.dto.CardRequest;
+import botobo.core.quiz.dto.CardResponse;
 import botobo.core.quiz.dto.NextQuizCardsRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -9,6 +11,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -31,10 +36,148 @@ public class CardAcceptanceTest extends DomainAcceptanceTest {
     }
 
     @Test
+    @DisplayName("카드 생성 - 성공")
+    void createCard() {
+        // given
+        CardRequest cardRequest = CardRequest.builder()
+                .question("question")
+                .answer("answer")
+                .workbookId(1L)
+                .build();
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .auth().oauth2(로그인되어_있음().getAccessToken())
+                        .body(cardRequest)
+                        .when().post("/api/cards")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        CardResponse cardResponse = response.as(CardResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(cardResponse).extracting("id").isNotNull();
+        assertThat(cardResponse).extracting("question").isEqualTo(cardRequest.getQuestion());
+        assertThat(cardResponse).extracting("answer").isEqualTo(cardRequest.getAnswer());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    @DisplayName("카드 생성 - 실패, 유효하지 않은 question")
+    void createCardWithInvalidQuestion(String question) {
+        // given
+        CardRequest cardRequest = CardRequest.builder()
+                .question(question)
+                .answer("answer")
+                .workbookId(1L)
+                .build();
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .auth().oauth2(로그인되어_있음().getAccessToken())
+                        .body(cardRequest)
+                        .when().post("/api/cards")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(errorResponse).extracting("message").isEqualTo("질문은 필수 입력값입니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    @DisplayName("카드 생성 - 실패, 유효하지 않은 answer")
+    void createCardWithInvalidAnswer(String answer) {
+        // given
+        CardRequest cardRequest = CardRequest.builder()
+                .question("question")
+                .answer(answer)
+                .workbookId(1L)
+                .build();
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .auth().oauth2(로그인되어_있음().getAccessToken())
+                        .body(cardRequest)
+                        .when().post("/api/cards")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(errorResponse).extracting("message").isEqualTo("답변은 필수 입력값입니다.");
+    }
+
+    @Test
+    @DisplayName("카드 생성 - 실패, 유효하지 않은 문제집 id")
+    void createCardWithInvalidWorkbookId() {
+        // given
+        CardRequest cardRequest = CardRequest.builder()
+                .question("question")
+                .answer("answer")
+                .workbookId(null)
+                .build();
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .auth().oauth2(로그인되어_있음().getAccessToken())
+                        .body(cardRequest)
+                        .when().post("/api/cards")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(errorResponse).extracting("message").isEqualTo("카드가 포함될 문제집 아이디는 필수 입력값입니다.");
+
+    }
+
+    @Test
+    @DisplayName("카드 생성 - 실패, 존재하지 않는 문제집 id")
+    void createCardWithNoneExistingWorkbookId() {
+        // given
+        Long noneExisingWorkbookId = 10000L;
+        CardRequest cardRequest = CardRequest.builder()
+                .question("question")
+                .answer("answer")
+                .workbookId(noneExisingWorkbookId)
+                .build();
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .auth().oauth2(로그인되어_있음().getAccessToken())
+                        .body(cardRequest)
+                        .when().post("/api/cards")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(errorResponse).extracting("message").isEqualTo("해당 문제집을 찾을 수 없습니다.");
+    }
+
+    @Test
     @DisplayName("또 보기 원하는 카드 선택 - 성공")
     void selectNextQuizCards() {
         // given
-        NextQuizCardsRequest request = NextQuizCardsRequest.builder()
+        NextQuizCardsRequest nextQuizCardsRequest = NextQuizCardsRequest.builder()
                 .cardIds(List.of(1L, 2L, 3L))
                 .build();
 
@@ -43,7 +186,7 @@ public class CardAcceptanceTest extends DomainAcceptanceTest {
                 RestAssured.given().log().all()
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .auth().oauth2(로그인되어_있음().getAccessToken())
-                        .body(request)
+                        .body(nextQuizCardsRequest)
                         .when().put("/api/cards/next-quiz")
                         .then().log().all()
                         .extract();
--- backend/src/test/java/botobo/core/quiz/application/CardServiceTest.java
+++ backend/src/test/java/botobo/core/quiz/application/CardServiceTest.java
@@ -3,6 +3,8 @@ package botobo.core.quiz.application;
 import botobo.core.quiz.domain.card.Card;
 import botobo.core.quiz.domain.card.CardRepository;
 import botobo.core.quiz.domain.workbook.Workbook;
+import botobo.core.quiz.domain.workbook.WorkbookRepository;
+import botobo.core.quiz.dto.CardRequest;
 import botobo.core.quiz.dto.NextQuizCardsRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -25,8 +29,34 @@ class CardServiceTest {
     @Mock
     private CardRepository cardRepository;
 
+    @Mock
+    private WorkbookRepository workbookRepository;
+
     @InjectMocks
     private CardService cardService;
+
+    @Test
+    @DisplayName("카드 생성 - 성공")
+    void createCard() {
+        // given
+        CardRequest cardRequest = cardRequest();
+        Workbook workbook = workbook(1L);
+        Card card = card(1L, workbook);
+
+        given(workbookRepository.findById(cardRequest.getWorkbookId())).willReturn(Optional.of(workbook));
+        given(cardRepository.save(any())).willReturn(card);
+
+        // when
+        cardService.createCard(cardRequest);
+
+        // then
+        then(workbookRepository)
+                .should(times(1))
+                .findById(cardRequest.getWorkbookId());
+        then(cardRepository)
+                .should(times(1))
+                .save(any());
+    }
 
     @Test
     @DisplayName("다음에 또 보는 카드 선택 - 성공")
@@ -47,15 +77,11 @@ class CardServiceTest {
                 .findByIdIn(anyList());
     }
 
-    private List<Card> listOfThreeCards() {
-        Workbook workbook = Workbook.builder()
+    private Workbook workbook(Long id) {
+        return Workbook.builder()
+                .id(id)
                 .name("workbook")
                 .build();
-        return List.of(
-                card(1L, workbook),
-                card(2L, workbook),
-                card(3L, workbook)
-        );
     }
 
     private Card card(Long id, Workbook workbook) {
@@ -64,6 +90,22 @@ class CardServiceTest {
                 .question("question")
                 .answer("answer")
                 .workbook(workbook)
+                .build();
+    }
+
+    private List<Card> listOfThreeCards() {
+        return List.of(
+                card(1L, workbook(1L)),
+                card(2L, workbook(1L)),
+                card(3L, workbook(1L))
+        );
+    }
+
+    private CardRequest cardRequest() {
+        return CardRequest.builder()
+                .question("question")
+                .answer("answer")
+                .workbookId(1L)
                 .build();
     }
 
