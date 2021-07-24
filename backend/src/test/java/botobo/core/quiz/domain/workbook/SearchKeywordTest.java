5872b29ad9b6ab6f061be10d1ab00b59d9be1a73
seunghyun <ggyool@naver.com>
2021-07-25 01:33:56 +0900

    feat: 검색어 객체인 SearchKeyword 생성

---
+++ backend/src/test/java/botobo/core/quiz/domain/workbook/SearchKeywordTest.java
@@ -0,0 +1,83 @@
+package botobo.core.quiz.domain.workbook;
+
+import botobo.core.quiz.exception.SearchKeywordCreationFailureException;
+import botobo.core.utils.TestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static botobo.core.utils.TestUtils.stringGenerator;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SearchKeywordTest {
+
+    @Test
+    @DisplayName("SearchKeyword 객체 생성 - 성공")
+    void create() {
+        // given
+        final int keywordMaxLength = 30;
+        String keyword = stringGenerator(keywordMaxLength);
+
+        // when
+        SearchKeyword searchKeyword = SearchKeyword.from(keyword);
+
+        // then
+        assertThat(searchKeyword).isEqualTo(
+                SearchKeyword.from(keyword)
+        );
+    }
+
+    @ValueSource(strings = {"   java", "java  ", "  java   "})
+    @ParameterizedTest
+    @DisplayName("SearchKeyword는 양 공백이 제거되어 생성된다.")
+    void createWithTrimming(String value) {
+        // given
+        SearchKeyword expected = SearchKeyword.from("java");
+
+        // when, then
+        assertThat(SearchKeyword.from(value)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("중간에 연속된 공백이 있으면 하나로 변경하여 생성한다.")
+    void createWithMultipleSpace() {
+        // given
+        final SearchKeyword expected = SearchKeyword.from("  j    a    v    a  ");
+
+        // when, then
+        assertThat(SearchKeyword.from("j a v a")).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("SearchKeyword 객체 생성 - 실패, 긴 문자열")
+    void createWithLongString() {
+        // when, then
+        assertThatThrownBy(() -> SearchKeyword.from(TestUtils.stringGenerator(31)))
+                .isInstanceOf(SearchKeywordCreationFailureException.class)
+                .hasMessageContaining("검색어는 30자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("SearchKeyword 객체 생성 - 실패, 금지어 포함")
+    void createWithForbiddenString() {
+        // when, then
+        assertThatThrownBy(() -> SearchKeyword.from("바보"))
+                .isInstanceOf(SearchKeywordCreationFailureException.class)
+                .hasMessageContaining("금지어를 입력했습니다");
+    }
+
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "    "})
+    @ParameterizedTest
+    @DisplayName("null, 빈 문자열, 공백 문자열은 캐싱 된 NO_SEARCH_KEYWORD 객체가 생성된다.")
+    void createWithNoKeyword(String value) {
+        // given
+        SearchKeyword noSearchKeyword = SearchKeyword.from(null);
+
+        // when, then
+        assertThat(SearchKeyword.from(value)).isSameAs(noSearchKeyword);
+    }
+}
