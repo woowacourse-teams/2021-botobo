18deb53d9122e4b0062e8dad44bdeda83d1c87f9
pkeugine <pkeugine@gmail.com>
2021-07-24 21:54:50 +0900

    docs: 카드 생성 및 수정 api 문서화

--- backend/src/test/java/botobo/core/documentation/CardDocumentationTest.java
+++ backend/src/test/java/botobo/core/documentation/CardDocumentationTest.java
@@ -2,6 +2,10 @@ package botobo.core.documentation;
 
 import botobo.core.auth.infrastructure.JwtTokenProvider;
 import botobo.core.quiz.application.CardService;
+import botobo.core.quiz.dto.CardRequest;
+import botobo.core.quiz.dto.CardResponse;
+import botobo.core.quiz.dto.CardUpdateRequest;
+import botobo.core.quiz.dto.CardUpdateResponse;
 import botobo.core.quiz.dto.NextQuizCardsRequest;
 import botobo.core.quiz.ui.CardController;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,8 +24,11 @@ import java.util.List;
 
 import static botobo.core.documentation.DocumentationUtils.getDocumentRequest;
 import static botobo.core.documentation.DocumentationUtils.getDocumentResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,6 +49,74 @@ public class CardDocumentationTest {
 
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
+
+    @Test
+    @DisplayName("카드 생성 - 성공")
+    void createCard() throws Exception {
+        // given
+        CardRequest request = CardRequest.builder()
+                .question("이것은 질문입니다!")
+                .answer("그리고 답변입니다~")
+                .workbookId(1L)
+                .build();
+        CardResponse response = CardResponse.builder()
+                .id(1L)
+                .question("이것은 질문입니다!")
+                .answer("그리고 답변입니다~")
+                .workbookId(1L)
+                .encounterCount(0)
+                .bookmark(false)
+                .nextQuiz(false)
+                .build();
+        String token = "botobo.access.token";
+        given(jwtTokenProvider.isValidToken(token)).willReturn(true);
+        given(cardService.createCard(any())).willReturn(response);
+
+        // when, then
+        mockMvc.perform(post("/api/cards")
+                .header("Authorization", "Bearer " + token)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andDo(document("cards-post-success",
+                        getDocumentRequest(),
+                        getDocumentResponse())
+                );
+    }
+
+    @Test
+    @DisplayName("카드 수정 - 성공")
+    void updateCard() throws Exception {
+        // given
+        CardUpdateRequest request = CardUpdateRequest.builder()
+                .question("수정된 질문입니다!")
+                .answer("그리고 수정된 답변입니다~")
+                .bookmark(true)
+                .build();
+        CardUpdateResponse response = CardUpdateResponse.builder()
+                .id(1L)
+                .question("수정된 질문입니다!")
+                .answer("그리고 수정된 답변입니다~")
+                .workbookId(1L)
+                .bookmark(true)
+                .build();
+        String token = "botobo.access.token";
+        given(jwtTokenProvider.isValidToken(token)).willReturn(true);
+        given(cardService.updateCard(anyLong(), any())).willReturn(response);
+
+        // when, then
+        mockMvc.perform(put("/api/cards/1")
+                .header("Authorization", "Bearer " + token)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(document("cards-put-success",
+                        getDocumentRequest(),
+                        getDocumentResponse())
+                );
+    }
 
     @Test
     @DisplayName("또 보기 원하는 카드 선택 - 성공")
