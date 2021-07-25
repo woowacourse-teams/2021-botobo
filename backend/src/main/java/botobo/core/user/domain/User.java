40ca554b83180743766cb168304e19725a1413d8
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-07-25 18:05:22 +0900

    feat: GithubUserInfoResponse에서 Role을 추가해서 User로 변환되도록 추가

--- backend/src/main/java/botobo/core/user/domain/User.java
+++ backend/src/main/java/botobo/core/user/domain/User.java
@@ -1,22 +1,16 @@
 package botobo.core.user.domain;
 
 import botobo.core.common.domain.BaseEntity;
-import botobo.core.quiz.domain.card.Card;
 import botobo.core.quiz.domain.workbook.Workbook;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,9 +30,9 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String profileUrl;
 
-    // 로그인 관련 테스트가 깨져서 일단 null 가능하게 해 두었음 (nullable = false)
     @Enumerated(EnumType.STRING)
-    private Role role = Role.USER;
+    @Column(nullable = false)
+    private Role role;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Workbook> workbooks = new ArrayList<>();
