11a81aadb57e624e71fbbfab93cc2963fdfb7775
seunghyun <ggyool@naver.com>
2021-07-27 23:48:08 +0900

    refactor: 문제집 조회와 검색에 필요한 필드만 반환하도록 수정

--- backend/src/main/java/botobo/core/dto/workbook/WorkbookResponse.java
+++ backend/src/main/java/botobo/core/dto/workbook/WorkbookResponse.java
@@ -1,6 +1,7 @@
 package botobo.core.dto.workbook;
 
 import botobo.core.domain.workbook.Workbook;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,8 +19,10 @@ public class WorkbookResponse {
     private Long id;
     private String name;
     private int cardCount;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String author;
-    private boolean opened;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean opened;
 
     public static WorkbookResponse of(Workbook workbook) {
         return WorkbookResponse.builder()
@@ -35,5 +38,35 @@ public class WorkbookResponse {
         return workbooks.stream()
                 .map(WorkbookResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    public static List<WorkbookResponse> ownedListOf(List<Workbook> workbooks) {
+        return workbooks.stream()
+                .map(WorkbookResponse::ownedOf)
+                .collect(Collectors.toList());
+    }
+
+    private static WorkbookResponse ownedOf(Workbook workbook) {
+        return WorkbookResponse.builder()
+                .id(workbook.getId())
+                .name(workbook.getName())
+                .cardCount(workbook.cardCount())
+                .opened(workbook.isOpened())
+                .build();
+    }
+
+    public static List<WorkbookResponse> openedListOf(List<Workbook> workbooks) {
+        return workbooks.stream()
+                .map(WorkbookResponse::openedOf)
+                .collect(Collectors.toList());
+    }
+
+    private static WorkbookResponse openedOf(Workbook workbook) {
+        return WorkbookResponse.builder()
+                .id(workbook.getId())
+                .name(workbook.getName())
+                .cardCount(workbook.cardCount())
+                .author(workbook.author())
+                .build();
     }
 }
