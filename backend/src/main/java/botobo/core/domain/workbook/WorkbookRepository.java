48fe6bfdbd54f52c0a9c825095ddc92de6f206c3
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-07-27 16:02:20 +0900

    feat: 최신순으로 Workbook이 조회되도록 수정 및 테스트 코드 추가

--- backend/src/main/java/botobo/core/domain/workbook/WorkbookRepository.java
+++ backend/src/main/java/botobo/core/domain/workbook/WorkbookRepository.java
@@ -1,6 +1,8 @@
 package botobo.core.domain.workbook;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,6 @@ public interface WorkbookRepository extends JpaRepository<Workbook, Long> {
 
     boolean existsById(Long id);
 
-    List<Workbook> findAllByUserId(Long userId);
+    @Query("select w from Workbook w where w.user.id = :userId order by w.createdAt desc")
+    List<Workbook> findAllByUserId(@Param("userId") Long userId);
 }
