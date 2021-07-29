94d08688ac4981518250d55878cbbe46d9c55dbb
pkeugine <pkeugine@gmail.com>
2021-07-30 00:21:58 +0900

    refactor: DataJpaTest 에서 show sql 설정 false 로 변경

--- backend/src/test/java/botobo/core/domain/user/UserRepositoryTest.java
+++ backend/src/test/java/botobo/core/domain/user/UserRepositoryTest.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 public class UserRepositoryTest {
 
     @Autowired
