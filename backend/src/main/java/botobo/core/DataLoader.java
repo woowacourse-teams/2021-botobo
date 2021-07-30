55db05182225159c6cb12b23a6662a8bd3a878cc
seunghyun <ggyool@naver.com>
2021-07-31 05:07:31 +0900

    etc: ddl, dml, DataLoader 추가

--- backend/src/main/java/botobo/core/DataLoader.java
+++ backend/src/main/java/botobo/core/DataLoader.java
@@ -2,6 +2,8 @@ package botobo.core;
 
 import botobo.core.domain.card.Card;
 import botobo.core.domain.card.CardRepository;
+import botobo.core.domain.tag.Tag;
+import botobo.core.domain.tag.Tags;
 import botobo.core.domain.user.Role;
 import botobo.core.domain.user.User;
 import botobo.core.domain.user.UserRepository;
@@ -20,6 +22,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -128,6 +131,7 @@ public class DataLoader implements CommandLineRunner {
                 .name(workbookName)
                 .user(author)
                 .opened(true)
+                .tags(Tags.from(Collections.singletonList(Tag.from(workbookName))))
                 .build();
         return workbookRepository.save(workbook);
     }
