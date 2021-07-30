956fda05585409f1d66eb7da4d9be64f3e182d84
seunghyun <ggyool@naver.com>
2021-07-31 00:36:04 +0900

    feat: 생성자 인자를 필드와 같은 타입으로 수정, null 테스트 추가

--- backend/src/main/java/botobo/core/domain/tag/TagNames.java
+++ backend/src/main/java/botobo/core/domain/tag/TagNames.java
@@ -1,26 +1,44 @@
 package botobo.core.domain.tag;
 
+import botobo.core.exception.tag.TagNamesCreationFailureException;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class TagNames {
 
     private final List<TagName> tagNames;
 
-    private TagNames(List<String> names) {
-        this.tagNames = distinctTagNames(names);
+    private TagNames(List<TagName> tagNames) {
+        validateTagNames(tagNames);
+        this.tagNames = distinctTagNames(tagNames);
+    }
+
+    private void validateTagNames(List<TagName> tagNames) {
+        if (Objects.isNull(tagNames)) {
+            throw new TagNamesCreationFailureException("tagNames 인자는 null이 될 수 없습니다");
+        }
+    }
+
+    private List<TagName> distinctTagNames(List<TagName> names) {
+        return names.stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public static TagNames from(List<String> names) {
-        return new TagNames(names);
+        validateNames(names);
+        return new TagNames(names.stream()
+                .map(TagName::from)
+                .collect(Collectors.toList()));
     }
 
-    private List<TagName> distinctTagNames(List<String> names) {
-        return names.stream()
-                .distinct()
-                .map(TagName::from)
-                .collect(Collectors.toList());
+    private static void validateNames(List<String> tagNames) {
+        if (Objects.isNull(tagNames)) {
+            throw new TagNamesCreationFailureException("names 인자는 null이 될 수 없습니다");
+        }
     }
 
     public List<TagName> toList() {
--- backend/src/test/java/botobo/core/domain/tag/TagNamesTest.java
+++ backend/src/test/java/botobo/core/domain/tag/TagNamesTest.java
@@ -1,5 +1,6 @@
 package botobo.core.domain.tag;
 
+import botobo.core.exception.tag.TagNamesCreationFailureException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TagNamesTest {
 
@@ -15,12 +17,24 @@ class TagNamesTest {
     void create() {
         // given
         List<String> names = Arrays.asList("자바", "자바스크립트", "리액트");
+
+        // when
         TagNames tagNames = TagNames.from(names);
 
-        // when, then
+        // then
         assertThat(tagNames.toList())
                 .extracting("value")
                 .containsExactly("자바", "자바스크립트", "리액트");
+    }
+
+    @Test
+    @DisplayName("TagNames 객체 생성 - 실패, null 입력")
+    void createWithNull() {
+        // when, then
+        assertThatThrownBy(() -> TagNames.from(null))
+                .isInstanceOf(TagNamesCreationFailureException.class)
+                .hasMessageContaining("TagNames객체 생성에 실패했습니다")
+                .hasMessageContaining("인자는 null이 될 수 없습니다");
     }
 
     @Test
@@ -28,9 +42,11 @@ class TagNamesTest {
     void createDistinctly() {
         // given
         List<String> names = Arrays.asList("자바", "자바스크립트", "리액트", "자바", "자바");
+
+        // when
         TagNames tagNames = TagNames.from(names);
 
-        // when, then
+        // then
         assertThat(tagNames.toList()).hasSize(3);
     }
 }
