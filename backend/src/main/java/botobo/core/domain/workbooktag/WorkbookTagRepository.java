d2dd2e04f2b795df4ea304aee5a26c0695376199
seunghyun <ggyool@naver.com>
2021-07-31 23:33:37 +0900

    feat: WorkbookTagRepository 추가

---
+++ backend/src/main/java/botobo/core/domain/workbooktag/WorkbookTagRepository.java
@@ -0,0 +1,6 @@
+package botobo.core.domain.workbooktag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkbookTagRepository extends JpaRepository<WorkbookTag, Long> {
+}
