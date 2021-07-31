8618a6901c4d7554cafe1f4dc2811bdeea3d418d
seunghyun <ggyool@naver.com>
2021-08-01 01:00:59 +0900

    refactor: 통합 테스트에서 데이터 로더가 실행되어 추가

--- backend/src/test/java/botobo/core/application/TagServiceTest.java
+++ backend/src/test/java/botobo/core/application/TagServiceTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TagServiceTest {
 
