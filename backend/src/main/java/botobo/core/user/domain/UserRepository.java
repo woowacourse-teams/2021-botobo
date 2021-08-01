391ad42b39ac8307e0ad2af21f3cfae685440d59
seovalue <seominjeong.dev@gmail.com>
2021-08-01 23:34:20 +0900

    etc: 충돌 해결

--- backend/src/main/java/botobo/core/user/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package botobo.core.user.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByGithubId(Long githubId);
-}
