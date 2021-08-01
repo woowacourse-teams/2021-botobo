2bdb85e664ce0b370659983f84d2c6e04e7fe380
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-08-01 15:10:10 +0900

    etc: react-ga 추가
    Co-authored-by: Kyle <mkitigy@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import React, { Suspense } from 'react';
+import ReactGA from 'react-ga';
+import { withRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import { theme } from './constants';
@@ -7,8 +9,21 @@ import { ModalProvider, SnackbarProvider } from './contexts';
 import GlobalStyle from './GlobalStyle';
 import Router from './Router';
 
+ReactGA.initialize('UA-203684869-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+const RouteChangeTracker = withRouter(({ history }) => {
+  history.listen((location) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  });
+
+  return null;
+});
+
 const App = () => (
   <ThemeProvider theme={theme}>
+    <RouteChangeTracker />
     <RecoilRoot>
       <SnackbarProvider>
         <ModalProvider>
