50f4d1468fd11a28a01ac950f635806f1c68584e
pkeugine <pkeugine@gmail.com>
2021-08-01 19:30:15 +0900

    refactor: WorkbookCardResponse 정적 팩토리 메서드 추가

--- backend/src/main/java/botobo/core/dto/workbook/WorkbookCardResponse.java
+++ backend/src/main/java/botobo/core/dto/workbook/WorkbookCardResponse.java
@@ -2,6 +2,8 @@ package botobo.core.dto.workbook;
 
 import botobo.core.domain.workbook.Workbook;
 import botobo.core.dto.card.CardResponse;
+import botobo.core.dto.tag.TagResponse;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,13 +20,29 @@ public class WorkbookCardResponse {
 
     private Long workbookId;
     private String workbookName;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer cardCount;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<TagResponse> tagResponses;
     private List<CardResponse> cards;
 
-    public static WorkbookCardResponse of(Workbook workbook) {
+    public static WorkbookCardResponse ofUserWorkbook(Workbook workbook) {
         List<CardResponse> cardResponses = CardResponse.listOf(workbook.getCards());
         return WorkbookCardResponse.builder()
                 .workbookId(workbook.getId())
                 .workbookName(workbook.getName())
+                .cards(cardResponses)
+                .build();
+    }
+
+    public static WorkbookCardResponse ofOpenedWorkbook(Workbook workbook) {
+        List<CardResponse> cardResponses = CardResponse.listOfSimple(workbook.getCards());
+        List<TagResponse> tagResponses = TagResponse.listOf(workbook.tags());
+        return WorkbookCardResponse.builder()
+                .workbookId(workbook.getId())
+                .workbookName(workbook.getName())
+                .cardCount(workbook.cardCount())
+                .tagResponses(tagResponses)
                 .cards(cardResponses)
                 .build();
     }
