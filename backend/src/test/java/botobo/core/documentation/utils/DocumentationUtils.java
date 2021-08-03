a6bfbe864e46993da0fd2949d7a2527f00f5eb82
seovalue <seominjeong.dev@gmail.com>
2021-08-03 22:03:53 +0900

    refactor: RequestBuilder 리팩토링 및 사용하지 않는 클래스 제거

--- backend/src/test/java/botobo/core/documentation/utils/DocumentationUtils.java
+++ backend/src/test/java/botobo/core/documentation/utils/DocumentationUtils.java
@@ -3,7 +3,6 @@ package botobo.core.documentation.utils;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -12,8 +11,7 @@ public class DocumentationUtils {
 
     public static OperationRequestPreprocessor getDocumentRequest() {
         return preprocessRequest(
-                prettyPrint(),
-                modifyUris().host("botobo.r-e.kr").removePort()
+                prettyPrint()
         );
     }
 
