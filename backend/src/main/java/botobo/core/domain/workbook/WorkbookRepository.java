32ed54eb4ce428107976e37e823b4b72960a59b0
pkeugine <pkeugine@gmail.com>
2021-08-05 17:18:44 +0900

    feat: 문제집 이름, 태그, 그리고 작성자 기준으로 문제집 조회 쿼리 추가

--- backend/src/main/java/botobo/core/domain/workbook/WorkbookRepository.java
+++ backend/src/main/java/botobo/core/domain/workbook/WorkbookRepository.java
@@ -17,4 +17,13 @@ public interface WorkbookRepository extends JpaRepository<Workbook, Long> {
 
     @Query("select w from Workbook w left join fetch w.cards.cards c where w.id = :id order by c.createdAt desc")
     Optional<Workbook> findByIdAndOrderCardByNew(@Param("id") Long id);
+
+    @Query("select w from Workbook w where w.name like %:name% and w.opened = true order by w.createdAt desc")
+    List<Workbook> findAllOpenedByNameAndOrderByNew(@Param("name") String name);
+
+    @Query("select w from Workbook w join fetch w.workbookTags wt where wt.tag.tagName.value = :tagName order by w.createdAt desc")
+    List<Workbook> findAllOpenedByTagAndOrderByNew(@Param("tagName") String tagName);
+
+    @Query("select w from Workbook w join fetch w.user u where u.userName = :authorName order by w.createdAt desc")
+    List<Workbook> findAllOpenedByAuthorAndOrderByNew(@Param("authorName") String authorName);
 }
