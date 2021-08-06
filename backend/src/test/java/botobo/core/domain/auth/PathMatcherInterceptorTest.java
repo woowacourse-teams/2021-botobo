5bc3fe8e11acf37c00c5f768bc869cf5f45c37bc
ohjoohyung <xntm3324@naver.com>
2021-08-06 14:37:18 +0900

    B41 - 경로와 HttpMethod를 이용해 인터셉터 설정 가능하도록 수정 및 /api/workbooks 경로를 인터셉터에 추가한다. (#322)
    * feat: 경로와 HttpMethod로 interceptor 설정 가능하도록 구현
    
    * refactor: 피드백 반영
    
    * test: PathMatcherInterceptor 관련 테스트 추가

---
+++ backend/src/test/java/botobo/core/domain/auth/PathMatcherInterceptorTest.java
@@ -0,0 +1,62 @@
+package botobo.core.domain.auth;
+
+import botobo.core.ui.auth.PathMatcherInterceptor;
+import botobo.core.ui.auth.PathMethod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@MockitoSettings
+public class PathMatcherInterceptorTest {
+
+    @Mock
+    private HandlerInterceptor handlerInterceptor;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @InjectMocks
+    private PathMatcherInterceptor pathMatcherInterceptor;
+
+    @Test
+    @DisplayName("동일한 api와 PathMethod를 추가시키면 preHandle은 다음 인터셉터의 preHandle을 반환한다.")
+    void preHandle() throws Exception {
+        // given
+        given(httpServletRequest.getRequestURI())
+                .willReturn("/api/**");
+        given(httpServletRequest.getMethod())
+                .willReturn(HttpMethod.GET.name());
+        given(handlerInterceptor.preHandle(any(), any(), any()))
+                .willReturn(true);
+        pathMatcherInterceptor.addPathPatterns("/api/**", PathMethod.GET);
+
+        // when, then
+        assertThat(pathMatcherInterceptor.preHandle(httpServletRequest, null, null)).isEqualTo(
+                handlerInterceptor.preHandle(any(), any(), any())
+        );
+    }
+
+    @Test
+    @DisplayName("모든 api 요청에서 OPTIONS일 때를 제외시키면 preHandle에서 true를 반환한다.")
+    void preHandleWithOptions() throws Exception {
+        // given
+        given(httpServletRequest.getRequestURI())
+                .willReturn("/api/workbooks");
+        given(httpServletRequest.getMethod())
+                .willReturn(HttpMethod.OPTIONS.toString());
+        pathMatcherInterceptor.excludePathPatterns("/api/**", PathMethod.OPTIONS);
+
+        // when, then
+        assertThat(pathMatcherInterceptor.preHandle(httpServletRequest, null, null)).isTrue();
+    }
+}
