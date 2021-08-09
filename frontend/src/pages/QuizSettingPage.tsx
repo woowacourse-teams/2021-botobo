429544c58455e5fac43ac3e19d12090455f175b5
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-08-09 20:31:07 +0900

    feat: 퀴즈 설정 페이지 UI 구현

--- frontend/src/pages/QuizSettingPage.tsx
+++ frontend/src/pages/QuizSettingPage.tsx
@@ -1,13 +1,26 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Button, MainHeader, WorkbookList } from '../components';
+import {
+  Button,
+  Checkbox,
+  MainHeader,
+  SelectBox,
+  WorkbookList,
+} from '../components';
 import { ROUTE } from '../constants';
 import { useQuizSetting } from '../hooks';
 
+const counts = [10, 15, 20, 25, 30].map((count, index) => ({
+  id: index,
+  name: count,
+}));
+
 const QuizSettingPage = () => {
   const { workbooks, checkWorkbook, startQuiz } = useQuizSetting();
+  const [selectedCountId, setSelectedCountId] = useState(counts[0].id);
+  const [isTimeCheck, setIsTimeCheck] = useState(false);
 
   return (
     <>
@@ -18,9 +31,23 @@ const QuizSettingPage = () => {
         <WorkbookWrapper>
           <WorkbookList workbooks={workbooks} onClickWorkbook={checkWorkbook} />
         </WorkbookWrapper>
-        <Button onClick={startQuiz} size="full">
+        <span>몇 문제를 풀어볼까요?</span>
+        <SelectBoxWrapper>
+          <SelectBox
+            optionValues={counts}
+            listHeight="8rem"
+            setSelectedId={(id) => setSelectedCountId(id)}
+          />
+        </SelectBoxWrapper>
+        <Checkbox
+          name="time-check"
+          labelText="시간도 확인하고 싶어요"
+          checked={isTimeCheck}
+          onChange={({ target }) => setIsTimeCheck(target.checked)}
+        />
+        <StyleButton onClick={startQuiz} size="full">
           시작!
-        </Button>
+        </StyleButton>
       </Container>
     </>
   );
@@ -40,6 +67,15 @@ const Title = styled.h2`
 const WorkbookWrapper = styled.div`
   margin-top: 1rem;
   margin-bottom: 2.5rem;
+`;
+
+const SelectBoxWrapper = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 3.5rem;
+`;
+
+const StyleButton = styled(Button)`
+  margin-top: 3.5rem;
 `;
 
 export default QuizSettingPage;
