ed60fe6ad775ab17fb84dc866ea08136b36a8349
pkeugine <pkeugine@gmail.com>
2021-08-10 18:25:00 +0900

    feat: sonarqube 도입 및 CORS 설정 변경

--- backend/src/main/java/botobo/core/config/WebConfig.java
+++ backend/src/main/java/botobo/core/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .maxAge(3000);
     }
