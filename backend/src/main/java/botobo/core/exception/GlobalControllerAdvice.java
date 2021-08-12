80802cfc260cf1765176deebab4d4aaaad2fa02f
seunghyun <ggyool@naver.com>
2021-08-12 12:50:44 +0900

    feat: Controller Advice의 로그 메시지 정리

--- backend/src/main/java/botobo/core/exception/GlobalControllerAdvice.java
+++ backend/src/main/java/botobo/core/exception/GlobalControllerAdvice.java
@@ -25,47 +25,46 @@ public class GlobalControllerAdvice {
         FieldError fieldError = e.getFieldError();
         Objects.requireNonNull(fieldError);
 
+        log.info(String.format("MethodArgumentNotValidException %s", e.getMessage()), e);
         ErrorType errorType = ErrorType.of(fieldError.getDefaultMessage());
-        log.info("MethodArgumentNotValidException", e);
         return ResponseEntity.badRequest().body(ErrorResponse.of(errorType));
     }
 
     @ExceptionHandler(InternalServerErrorException.class)
     public ResponseEntity<ErrorResponse> handleInternalServerErrorException(InternalServerErrorException e) {
-        log.info("InternalServerErrorException", e.getServerMessage());
+        log.info(String.format("InternalServerErrorException %s", e.getMessage()), e);
         ErrorType errorType = ErrorType.of(e.getClass());
         return ResponseEntity.status(e.getHttpStatus()).body(ErrorResponse.of(errorType));
     }
 
     @ExceptionHandler(BotoboException.class)
     public ResponseEntity<ErrorResponse> handleBotoboException(BotoboException e) {
-        log.info(e.getClass().getName(), e);
+        log.info(String.format("%s %s", e.getClass().getName(), e.getMessage()), e);
         return ResponseEntity.status(e.getHttpStatus()).body(ErrorResponse.of(e.getErrorType()));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException e) {
+        log.info(String.format("MissingServletRequestParameterException %s", e.getMessage()), e);
         String message = String.format("파라미터를 입력해야 합니다.(%s)", e.getParameterName());
-        log.info("MissingServletRequestParameterException", e);
         return ResponseEntity.badRequest().body(ErrorResponse.of("E001", message));
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceedException(MaxUploadSizeExceededException e) {
-        log.info("MaxUploadSizeExceededException", e);
-
+        log.info(String.format("MaxUploadSizeExceededException %s", e.getMessage()), e);
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(ErrorResponse.of(ErrorType.U010));
     }
 
     @ExceptionHandler(SizeLimitExceededException.class)
     public ResponseEntity<ErrorResponse> handleSizeLimitExceededException(SizeLimitExceededException e) {
-        log.info("SizeLimitExceededException", e);
+        log.info(String.format("SizeLimitExceededException %s", e.getMessage()), e);
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(ErrorResponse.of(ErrorType.U011));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.info("Exception", e.getStackTrace());
+        log.info(String.format("Exception %s", e.getMessage()), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.of(ErrorType.X001));
     }
 }
