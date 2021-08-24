1371531ad0b23f5a181956600fd896469c81c044
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-08-24 16:09:07 +0900

    hotfix/421: release 버전 1.0.4로 변경 (#422)

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main-103.js',
+    filename: 'main-104.js',
     publicPath: '/',
   },
   plugins: [
