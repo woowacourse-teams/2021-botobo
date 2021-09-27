7efa7420844a8e3b68a8b85be066aef4c3cb830c
seovalue <seominjeong.dev@gmail.com>
2021-09-27 15:42:36 +0900

    etc: FakeS3Uploader 사용하지 않는 어노테이션 제거

--- backend/src/main/java/botobo/core/infrastructure/s3/FakeS3Uploader.java
+++ backend/src/main/java/botobo/core/infrastructure/s3/FakeS3Uploader.java
@@ -1,15 +1,11 @@
 package botobo.core.infrastructure.s3;
 
 import botobo.core.domain.user.User;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Profile({"local", "test"})
-@Slf4j
-@RequiredArgsConstructor
 @Component
 public class FakeS3Uploader implements FileUploader {
     @Override
