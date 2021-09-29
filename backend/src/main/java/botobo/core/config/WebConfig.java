8cca6ad4a5372d22c45381a38e00ca9bf8445298
seovalue <seominjeong.dev@gmail.com>
2021-09-29 15:36:36 +0900

    refactor: Json Pretty Print 설정을 local, test에서만 적용하도록 변경

--- backend/src/main/java/botobo/core/config/WebConfig.java
+++ backend/src/main/java/botobo/core/config/WebConfig.java
@@ -1,15 +1,25 @@
 package botobo.core.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final Environment environment;
+
+    public WebConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -21,11 +31,21 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        for (HttpMessageConverter<?> converter : converters) {
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
-                jacksonConverter.setPrettyPrint(true);
+        if (isLocal() || isTest()) {
+            for (HttpMessageConverter<?> converter : converters) {
+                if (converter instanceof MappingJackson2HttpMessageConverter) {
+                    MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
+                    jacksonConverter.setPrettyPrint(true);
+                }
             }
         }
+    }
+
+    private boolean isLocal() {
+        return Arrays.asList(environment.getActiveProfiles()).contains("local");
+    }
+
+    private boolean isTest() {
+        return Arrays.asList(environment.getActiveProfiles()).contains("test");
     }
 }
