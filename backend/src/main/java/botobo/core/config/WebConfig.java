93581cf51b9406a2b43c2e5052b5f68b129565e6
서민정 <seominjeong.dev@gmail.com>
2021-10-01 10:37:01 +0900

    B07 - 비동기 로깅 적용 및 로깅 가독성있게 개선하기 (#550)
    * refactor: Json Pretty Print 설정을 local, test에서만 적용하도록 변경
    
    * feat: query pattern 변경
    
    * refactor: file과 console 패턴 분리
    
    * feat: AsyncAppender 도입
    
    * refactor: utf8 제거
    
    * refactor: AsyncAppender 설정 변경
    
    * refactor: Config 파일 분리

--- backend/src/main/java/botobo/core/config/WebConfig.java
+++ backend/src/main/java/botobo/core/config/WebConfig.java
@@ -1,11 +1,16 @@
 package botobo.core.config;
 
+import botobo.core.ui.search.SearchArgumentResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -20,12 +25,13 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        for (HttpMessageConverter<?> converter : converters) {
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
-                jacksonConverter.setPrettyPrint(true);
-            }
-        }
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(searchArgumentResolver());
     }
+
+    @Bean
+    public SearchArgumentResolver searchArgumentResolver() {
+        return new SearchArgumentResolver();
+    }
+
 }
