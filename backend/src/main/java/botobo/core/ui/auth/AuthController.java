e22a2c5f7c93dc54f60f1aaa5760975869446c6d
Eugine Park <pkeugine@gmail.com>
2021-10-04 04:15:59 +0900

    feat: AuthController에 token 재발급 api와 로그아웃 api 추가 및 문서화 테스트 추가

--- backend/src/main/java/botobo/core/ui/auth/AuthController.java
+++ backend/src/main/java/botobo/core/ui/auth/AuthController.java
@@ -3,26 +3,76 @@ package botobo.core.ui.auth;
 import botobo.core.application.AuthService;
 import botobo.core.dto.auth.LoginRequest;
 import botobo.core.dto.auth.TokenResponse;
+import botobo.core.infrastructure.auth.JwtRefreshTokenInfo;
+import botobo.core.infrastructure.auth.JwtTokenType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("/login")
 public class AuthController {
 
-    private final AuthService authService;
+    private static final String REFRESH_TOKEN_COOKIE_NAME = "BTOKEN_REFRESH";
 
-    public AuthController(AuthService authService) {
+    private final AuthService authService;
+    private final JwtRefreshTokenInfo jwtRefreshTokenInfo;
+
+    public AuthController(AuthService authService, JwtRefreshTokenInfo jwtRefreshTokenInfo) {
         this.authService = authService;
+        this.jwtRefreshTokenInfo = jwtRefreshTokenInfo;
     }
 
-    @PostMapping("/{socialType}")
-    public ResponseEntity<TokenResponse> login(@PathVariable String socialType, @RequestBody LoginRequest loginRequest) {
-        TokenResponse tokenResponse = authService.createToken(socialType, loginRequest);
+    @PostMapping("/login/{socialType}")
+    public ResponseEntity<TokenResponse> login(@PathVariable String socialType, @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        TokenResponse tokenResponse = authService.createAccessToken(socialType, loginRequest);
+        Long id = authService.extractIdByToken(tokenResponse.getAccessToken(), JwtTokenType.ACCESS_TOKEN);
+        Cookie cookie = createRefreshTokenCookie(id);
+        response.addCookie(cookie);
         return ResponseEntity.ok(tokenResponse);
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<TokenResponse> renewToken(@CookieValue(value = REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken, HttpServletResponse response) {
+        authService.validateRefreshToken(refreshToken);
+        Long id = authService.extractIdByToken(refreshToken, JwtTokenType.REFRESH_TOKEN);
+        TokenResponse tokenResponse = authService.renewAccessToken(id);
+        Cookie cookie = createRefreshTokenCookie(id);
+        response.addCookie(cookie);
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    private Cookie createRefreshTokenCookie(Long id) {
+        String refreshToken = authService.createRefreshToken(id);
+        Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(jwtRefreshTokenInfo.getValidityInMilliseconds().intValue());
+        return cookie;
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(@CookieValue(value = REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken,
+                                       HttpServletResponse response) {
+
+        authService.removeRefreshToken(refreshToken);
+        expireRefreshTokenCookie(response);
+        return ResponseEntity.noContent().build();
+    }
+
+    private void expireRefreshTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, "");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
--- backend/src/test/java/botobo/core/documentation/LoginDocumentationTest.java
+++ backend/src/test/java/botobo/core/documentation/LoginDocumentationTest.java
@@ -2,11 +2,17 @@ package botobo.core.documentation;
 
 import botobo.core.dto.auth.LoginRequest;
 import botobo.core.dto.auth.TokenResponse;
+import botobo.core.infrastructure.auth.JwtTokenType;
 import botobo.core.ui.auth.AuthController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
 
+import javax.servlet.http.Cookie;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -14,23 +20,95 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("로그인 문서화 테스트")
 @WebMvcTest(AuthController.class)
-public class LoginDocumentationTest extends DocumentationTest {
+class LoginDocumentationTest extends DocumentationTest {
 
     @Test
     @DisplayName("로그인 - 성공")
     void login() throws Exception {
         //given
         LoginRequest loginRequest = new LoginRequest("authCode");
-        given(authService.createToken(anyString(), any(LoginRequest.class))).willReturn(
+        String refreshToken = "botobo.refresh.token";
+        Long id = authenticatedUser().getId();
+
+        given(authService.extractIdByToken(authenticatedToken(), JwtTokenType.ACCESS_TOKEN)).willReturn(id);
+        given(authService.createAccessToken(anyString(), any(LoginRequest.class))).willReturn(
                 TokenResponse.of(authenticatedToken())
         );
+        given(authService.createRefreshToken(id)).willReturn(refreshToken);
+        given(jwtRefreshTokenInfo.getValidityInMilliseconds()).willReturn(1000L);
 
         // when, then
-        document()
+        MvcResult mvcResult = document()
                 .mockMvc(mockMvc)
                 .post("/login/github", loginRequest)
                 .build()
                 .status(status().isOk())
                 .identifier("login-success");
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        Cookie cookie = response.getCookie("BTOKEN_REFRESH");
+        assertThat(cookie.getValue()).isEqualTo(refreshToken);
+        assertThat(cookie.isHttpOnly()).isTrue();
+        assertThat(cookie.getSecure()).isTrue();
+        assertThat(cookie.getPath()).isEqualTo("/");
+        assertThat(cookie.getMaxAge()).isPositive();
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰을 이용하여 액세스 토큰 재발급 - 성공")
+    void renewToken() throws Exception {
+        //given
+        String refreshToken = "botobo.refresh.token";
+        String renewedAccessToken = "new.access.token";
+        String renewedRefreshToken = "new.refresh.token";
+        Long id = authenticatedUser().getId();
+        given(authService.extractIdByToken(refreshToken, JwtTokenType.REFRESH_TOKEN)).willReturn(id);
+        given(authService.renewAccessToken(id)).willReturn(
+                TokenResponse.of(renewedAccessToken)
+        );
+        given(authService.createRefreshToken(id)).willReturn(renewedRefreshToken);
+        given(jwtRefreshTokenInfo.getValidityInMilliseconds()).willReturn(1000L);
+
+        // when, then
+        MvcResult mvcResult = document()
+                .mockMvc(mockMvc)
+                .get("/token")
+                .cookie(new Cookie("BTOKEN_REFRESH", refreshToken))
+                .build()
+                .status(status().isOk())
+                .identifier("token-get-success");
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        Cookie cookie = response.getCookie("BTOKEN_REFRESH");
+        assertThat(cookie.getValue()).isEqualTo(renewedRefreshToken);
+        assertThat(cookie.isHttpOnly()).isTrue();
+        assertThat(cookie.getSecure()).isTrue();
+        assertThat(cookie.getPath()).isEqualTo("/");
+        assertThat(cookie.getMaxAge()).isPositive();
+    }
+
+    @Test
+    @DisplayName("로그아웃 - 성공")
+    void logout() throws Exception {
+        //given
+        String refreshToken = "botobo.refresh.token";
+
+        // when, then
+        MvcResult mvcResult = document()
+                .mockMvc(mockMvc)
+                .get("/logout")
+                .auth(authenticatedToken())
+                .cookie(new Cookie("BTOKEN_REFRESH", refreshToken))
+                .build()
+                .status(status().isNoContent())
+                .identifier("logout-get-success");
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        Cookie cookie = response.getCookie("BTOKEN_REFRESH");
+        assertThat(cookie.getValue()).isBlank();
+        assertThat(cookie.isHttpOnly()).isTrue();
+        assertThat(cookie.getSecure()).isTrue();
+        assertThat(cookie.getPath()).isEqualTo("/");
+        assertThat(cookie.getMaxAge()).isZero();
     }
 }
