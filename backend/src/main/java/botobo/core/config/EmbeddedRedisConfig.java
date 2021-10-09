e4e0e9fb09451210efe0b30689f30b60ac071b54
seovalue <seominjeong.dev@gmail.com>
2021-10-09 17:45:12 +0900

    feat: EmbeddedRedisConfig M1 Mac 대응

--- backend/src/main/java/botobo/core/config/EmbeddedRedisConfig.java
+++ backend/src/main/java/botobo/core/config/EmbeddedRedisConfig.java
@@ -8,8 +8,12 @@ import redis.embedded.RedisServer;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Objects;
 
 @Configuration
 @Profile({"local", "test"})
@@ -21,10 +25,29 @@ public class EmbeddedRedisConfig {
     private RedisServer redisServer;
 
     @PostConstruct
-    public void redisServer() throws IOException {
+    public void redisServer() throws IOException, URISyntaxException {
         int port = isRedisRunning() ? findAvailablePort() : redisPort;
-        redisServer = new RedisServer(port);
+
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArcMac(), port);
+        } else {
+            redisServer = new RedisServer(port);
+        }
+
         redisServer.start();
+    }
+
+    private File getRedisFileForArcMac() throws URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("binary/redis/redis-server-6.2.5-mac-arm64");
+        assert resource != null;
+        File file = new File(resource.toURI());
+        file.setExecutable(true);
+        return file;
+    }
+
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64") &&
+                Objects.equals(System.getProperty("os.name"), "Mac OS X");
     }
 
     @PreDestroy
@@ -83,7 +106,7 @@ public class EmbeddedRedisConfig {
                 pidInfo.append(line);
             }
 
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
 
         return pidInfo.toString().length() != 0;
