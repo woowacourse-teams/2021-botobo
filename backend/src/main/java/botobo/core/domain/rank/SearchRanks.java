256343dc88ecdcc6228a8a592270a60b8e50bc00
seunghyun <ggyool@naver.com>
2021-10-13 05:54:00 +0900

    feat: SearchRanks 일급컬렉션 추가

---
+++ backend/src/main/java/botobo/core/domain/rank/SearchRanks.java
@@ -0,0 +1,39 @@
+package botobo.core.domain.rank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class SearchRanks {
+
+    private final List<SearchRank> searchRanks;
+
+    public SearchRanks(List<SearchRank> searchRanks) {
+        this.searchRanks = searchRanks;
+    }
+
+    public List<SearchRank> makeNewSearchRanks(Set<String> keywords) {
+        List<SearchRank> newSearchRanks = new ArrayList<>();
+        int newRank = 1;
+        for (String keyword : keywords) {
+            newSearchRanks.add(makeNewSearchRank(keyword, newRank++));
+        }
+        return newSearchRanks;
+    }
+
+    private SearchRank makeNewSearchRank(String keyword, int newRank) {
+        if (findRank(keyword).isEmpty()) {
+            return new SearchRank(keyword, newRank, null);
+        }
+        Integer change = findRank(keyword).get() - newRank;
+        return new SearchRank(keyword, newRank, change);
+    }
+
+    private Optional<Integer> findRank(String keyword) {
+        return searchRanks.stream()
+                .filter(searchRank -> searchRank.isSameKeyword(keyword))
+                .findAny()
+                .map(SearchRank::getRank);
+    }
+}
