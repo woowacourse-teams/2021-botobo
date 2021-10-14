325f85e4c81d9d233abab508a570b3ec87a6bfb5
Eugine Park <pkeugine@gmail.com>
2021-10-15 02:22:43 +0900

    refactor: WorkbookSearchParameterUtils 빌더 생성 방식 변경

--- backend/src/test/java/botobo/core/utils/WorkbookSearchParameterUtils.java
+++ backend/src/test/java/botobo/core/utils/WorkbookSearchParameterUtils.java
@@ -4,44 +4,45 @@ import botobo.core.ui.search.WorkbookSearchParameter;
 
 public class WorkbookSearchParameterUtils {
 
-    public static Builder builder() {
-        return new Builder();
+    private String searchKeyword;
+    private String searchCriteria;
+    private String start;
+    private String size;
+
+    private WorkbookSearchParameterUtils() {
+
     }
 
-    public static class Builder {
+    public static WorkbookSearchParameterUtils builder() {
+        return new WorkbookSearchParameterUtils();
+    }
 
-        private String searchKeyword;
-        private String searchCriteria;
-        private String start;
-        private String size;
+    public WorkbookSearchParameterUtils searchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+        return this;
+    }
 
-        public Builder searchKeyword(String searchKeyword) {
-            this.searchKeyword = searchKeyword;
-            return this;
-        }
+    public WorkbookSearchParameterUtils searchCriteria(String searchCriteria) {
+        this.searchCriteria = searchCriteria;
+        return this;
+    }
 
-        public Builder searchCriteria(String searchCriteria) {
-            this.searchCriteria = searchCriteria;
-            return this;
-        }
+    public WorkbookSearchParameterUtils start(String start) {
+        this.start = start;
+        return this;
+    }
 
-        public Builder start(String start) {
-            this.start = start;
-            return this;
-        }
+    public WorkbookSearchParameterUtils size(String size) {
+        this.size = size;
+        return this;
+    }
 
-        public Builder size(String size) {
-            this.size = size;
-            return this;
-        }
-
-        public WorkbookSearchParameter build() {
-            return WorkbookSearchParameter.ofRequest(
-                    searchCriteria,
-                    searchKeyword,
-                    start,
-                    size
-            );
-        }
+    public WorkbookSearchParameter build() {
+        return WorkbookSearchParameter.ofRequest(
+                searchCriteria,
+                searchKeyword,
+                start,
+                size
+        );
     }
 }
