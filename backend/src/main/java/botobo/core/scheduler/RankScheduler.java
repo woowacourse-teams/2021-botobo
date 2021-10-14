15eb3242b27f6a4ffd147407cea7bc9813c83f0e
Eugine Park <pkeugine@gmail.com>
2021-10-14 17:38:53 +0900

    feat: 인기 문제집 캐싱된 값을 10분에 한 번씩 지우는 스케쥴러 설정 추가

--- backend/src/main/java/botobo/core/scheduler/RankScheduler.java
+++ backend/src/main/java/botobo/core/scheduler/RankScheduler.java
@@ -1,11 +1,24 @@
 package botobo.core.scheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RankScheduler {
+
+    private static final Logger log = LoggerFactory.getLogger(RankScheduler.class);
 
     @Scheduled(cron = "0 0 4 * * *")
     public void updateRanks() {
 
+    }
+
+    @Scheduled(cron = "0 0/10 * * * *")
+    @CacheEvict("workbookRanks")
+    public void updateWorkbookRanks() {
+        log.info("cleared cache for workbook rankings request");
     }
 }
