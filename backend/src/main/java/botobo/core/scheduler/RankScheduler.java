1d57934c34b90decd5c6321cf6b0ef6f1af95b27
seunghyun <ggyool@naver.com>
2021-10-14 18:06:23 +0900

    feat: RankScheduler 추가

--- backend/src/main/java/botobo/core/scheduler/RankScheduler.java
+++ backend/src/main/java/botobo/core/scheduler/RankScheduler.java
@@ -1,11 +1,47 @@
 package botobo.core.scheduler;
 
+import botobo.core.application.rank.SearchRankService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Profile({"local", "dev", "prod"})
+@Component
 public class RankScheduler {
 
-    @Scheduled(cron = "0 0 4 * * *")
-    public void updateRanks() {
+    @Value("${spring.config.activate.on-profile}")
+    private String profileName;
 
+    @Value("${scheduler.rank.hostname}")
+    private String hostName;
+
+    private final SearchRankService searchRankService;
+
+    public RankScheduler(SearchRankService searchRankService) {
+        this.searchRankService = searchRankService;
+    }
+
+    @Scheduled(cron = "0 0 4 * * *")
+    public void updateSearchRanks() {
+        if (isLocalProfile() || isIntendedHostName()) {
+            searchRankService.updateSearchRanks();
+        }
+    }
+
+    private boolean isLocalProfile() {
+        return "local".equals(profileName);
+    }
+
+    private boolean isIntendedHostName() {
+        try {
+            String runningHostName = InetAddress.getLocalHost().getHostName();
+            return hostName.equals(runningHostName);
+        } catch (UnknownHostException e) {
+            return false;
+        }
     }
 }
