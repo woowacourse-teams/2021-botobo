2c2417a7f459e1645913e0736be2b23d6a0a4df7
seunghyun <ggyool@naver.com>
2021-10-15 01:08:46 +0900

    test: 캐시 관련 테스트 추가

--- backend/src/main/java/botobo/core/config/CacheConfig.java
+++ backend/src/main/java/botobo/core/config/CacheConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"local", "dev", "prod"})
+//@Profile({"local", "dev", "prod"})
 @Configuration
 @EnableCaching
 public class CacheConfig {
--- backend/src/test/java/botobo/core/application/rank/SearchRankServiceTest.java
+++ backend/src/test/java/botobo/core/application/rank/SearchRankServiceTest.java
@@ -7,16 +7,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class SearchRankServiceTest {
+
+    private static final String SEARCH_RANKS_CACHE_VALUE = "SearchRanks";
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
@@ -26,6 +34,10 @@ class SearchRankServiceTest {
 
     @BeforeEach
     void setUp() {
+        // 주석은 나중에 지우겠음
+        // 캐시를 지우고 이전 검색 기록이 있는 상황 (일반적인 상황)
+        // 으로 만들어 놓는다.
+        getSearchRanksCache().clear();
         setScore("java", 10);
         setScore("spring", 5);
         setScore("react", 7);
@@ -39,12 +51,19 @@ class SearchRankServiceTest {
     }
 
     @Test
-    @DisplayName("검색 랭크를 가져온다 - 성공")
+    @DisplayName("검색 순위를 가져온다 - 성공")
     void bringSearchRanks() {
+        // given
+        // 검색 이전에는 캐시가 비어있다.
+        assertThat(isCacheEmpty()).isTrue();
+
         // when
         List<SearchRankResponse> searchRankResponses = searchRankService.bringSearchRanks();
 
         // then
+        // 검색 후 캐시가 채워진다.
+        assertThat(isCacheEmpty()).isFalse();
+
         assertThat(searchRankResponses).extracting(SearchRankResponse::getKeyword)
                 .containsExactly("java", "react", "spring");
         assertThat(searchRankResponses).extracting(SearchRankResponse::getRank)
@@ -54,9 +73,13 @@ class SearchRankServiceTest {
     }
 
     @Test
-    @DisplayName("검색 랭크를 업데이트한다 - 성공")
+    @DisplayName("검색 순위를 업데이트한다 - 성공")
     void updateSearchRanks() {
         // given
+        // update를 하면 캐시가 지워진다는 것을 테스트하기 위해 캐시를 채워 놓는다.
+        searchRankService.bringSearchRanks();
+        assertThat(isCacheEmpty()).isFalse();
+
         setScore("react", 11);
         setScore("java", 5);
         setScore("redis", 7);
@@ -66,6 +89,9 @@ class SearchRankServiceTest {
         searchRankService.updateSearchRanks();
 
         // then
+        // update 후에는 캐시가 비워져 있다.
+        assertThat(isCacheEmpty()).isTrue();
+
         List<SearchRankResponse> searchRankResponses = searchRankService.bringSearchRanks();
         assertThat(searchRankResponses).extracting(SearchRankResponse::getKeyword)
                 .containsExactly("react", "redis", "java");
@@ -73,6 +99,16 @@ class SearchRankServiceTest {
                 .containsExactly(1, 2, 3);
         assertThat(searchRankResponses).extracting(SearchRankResponse::getChange)
                 .containsExactly(1, null, -2);
+    }
+
+    private Cache getSearchRanksCache() {
+        return cacheManager.getCache(SEARCH_RANKS_CACHE_VALUE);
+    }
+
+    private boolean isCacheEmpty() {
+        // 이것보다 더 좋은 방법 없나 ?
+        Map<Object, Object> map = (Map<Object, Object>) getSearchRanksCache().getNativeCache();
+        return map.isEmpty();
     }
 
     private void setScore(String searchKeyword, int score) {
