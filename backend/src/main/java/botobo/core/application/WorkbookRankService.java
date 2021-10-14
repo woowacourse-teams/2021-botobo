f82a2cc059998f2d1bc3f7df4d5be39e568d03aa
Eugine Park <pkeugine@gmail.com>
2021-10-15 03:41:56 +0900

    refactor: Slf4j 롬복 어노테이션 적용

--- backend/src/main/java/botobo/core/application/WorkbookRankService.java
+++ backend/src/main/java/botobo/core/application/WorkbookRankService.java
@@ -6,6 +6,7 @@ import botobo.core.dto.workbook.WorkbookResponse;
 import botobo.core.ui.search.SearchCriteria;
 import botobo.core.ui.search.SearchKeyword;
 import botobo.core.ui.search.WorkbookSearchParameter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
@@ -15,11 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 public class WorkbookRankService {
-
-    private static final Logger log = LoggerFactory.getLogger(WorkbookRankService.class);
 
     private static final int START = 0;
     private static final int SIZE = 3;
