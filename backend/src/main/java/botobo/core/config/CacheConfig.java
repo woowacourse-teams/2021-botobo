577d1ad93d72f8c3069ff3f05b71c4fb59d91c9a
seunghyun <ggyool@naver.com>
2021-10-15 03:35:43 +0900

    refactor: 상황에 맞게 Scheduling 하는 로직을 @Conditional 을 사용하도록 수정

--- backend/src/main/java/botobo/core/config/CacheConfig.java
+++ backend/src/main/java/botobo/core/config/CacheConfig.java
@@ -5,9 +5,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-//@Profile({"local", "dev", "prod"})
 @Configuration
 @EnableCaching
 public class CacheConfig {
--- backend/src/main/java/botobo/core/scheduler/RankScheduler.java
+++ backend/src/main/java/botobo/core/scheduler/RankScheduler.java
@@ -1,23 +1,15 @@
 package botobo.core.scheduler;
 
 import botobo.core.application.rank.SearchRankService;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
+@Conditional(RankSchedulerCondition.class)
 @Profile({"local", "dev", "prod"})
 @Component
 public class RankScheduler {
-
-    @Value("${spring.config.activate.on-profile}")
-    private String profileName;
-
-    @Value("${scheduler.rank.hostname:}")
-    private String hostName;
 
     private final SearchRankService searchRankService;
 
@@ -27,21 +19,6 @@ public class RankScheduler {
 
     @Scheduled(cron = "0 0 4 * * *")
     public void updateSearchRanks() {
-        if (isLocalProfile() || isIntendedHostName()) {
-            searchRankService.updateSearchRanks();
-        }
-    }
-
-    private boolean isLocalProfile() {
-        return "local".equals(profileName);
-    }
-
-    private boolean isIntendedHostName() {
-        try {
-            String runningHostName = InetAddress.getLocalHost().getHostName();
-            return hostName.equals(runningHostName);
-        } catch (UnknownHostException e) {
-            return false;
-        }
+        searchRankService.updateSearchRanks();
     }
 }
