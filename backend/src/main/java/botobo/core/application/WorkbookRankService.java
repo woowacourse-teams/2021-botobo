a76518a07a4df131c654fd8292eb79db1acc95c3
Eugine Park <pkeugine@gmail.com>
2021-10-14 17:52:58 +0900

    feat: 환경에 따라 스케쥴러 동작 유무 설정

--- backend/src/main/java/botobo/core/application/WorkbookRankService.java
+++ backend/src/main/java/botobo/core/application/WorkbookRankService.java
@@ -6,6 +6,9 @@ import botobo.core.dto.workbook.WorkbookResponse;
 import botobo.core.ui.search.SearchCriteria;
 import botobo.core.ui.search.SearchKeyword;
 import botobo.core.ui.search.WorkbookSearchParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,8 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class WorkbookRankService {
+
+    private static final Logger log = LoggerFactory.getLogger(WorkbookRankService.class);
 
     private static final int INDEX = 0;
     private static final int SIZE = 3;
@@ -44,5 +49,10 @@ public class WorkbookRankService {
         );
         List<Workbook> workbooks = workbookPages.getContent();
         return WorkbookResponse.openedListOf(workbooks);
+    }
+
+    @CacheEvict("workbookRanks")
+    public void removeWorkbookRanksCache() {
+        log.info("cleared cache for workbook rankings request");
     }
 }
--- backend/src/main/java/botobo/core/scheduler/RankScheduler.java
+++ backend/src/main/java/botobo/core/scheduler/RankScheduler.java
@@ -1,15 +1,28 @@
 package botobo.core.scheduler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import botobo.core.application.WorkbookRankService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Component
 public class RankScheduler {
 
-    private static final Logger log = LoggerFactory.getLogger(RankScheduler.class);
+    @Value("${spring.config.activate.on-profile}")
+    private String profileName;
+
+    @Value("${rank.scheduler.hostname:}")
+    private String hostName;
+
+    private final WorkbookRankService workbookRankService;
+
+    public RankScheduler(WorkbookRankService workbookRankService) {
+        this.workbookRankService = workbookRankService;
+    }
 
     @Scheduled(cron = "0 0 4 * * *")
     public void updateRanks() {
@@ -19,6 +32,21 @@ public class RankScheduler {
     @Scheduled(cron = "0 0/10 * * * *")
     @CacheEvict("workbookRanks")
     public void updateWorkbookRanks() {
-        log.info("cleared cache for workbook rankings request");
+        if (isLocalProfile() || isIntendedHostName()) {
+            workbookRankService.removeWorkbookRanksCache();
+        }
+    }
+
+    private boolean isLocalProfile() {
+        return "local".equals(profileName);
+    }
+
+    private boolean isIntendedHostName() {
+        try {
+            String runningHostName = InetAddress.getLocalHost().getHostName();
+            return hostName.equals(runningHostName);
+        } catch (UnknownHostException e) {
+            return false;
+        }
     }
 }
