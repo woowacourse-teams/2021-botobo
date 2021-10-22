cea5ff36da5d4712b1419cb41754f2919d057d13
seovalue <seominjeong.dev@gmail.com>
2021-10-22 19:41:52 +0900

    etc: 코드 정리

--- backend/src/test/java/botobo/core/domain/tag/TagRepositoryTest.java
+++ backend/src/test/java/botobo/core/domain/tag/TagRepositoryTest.java
@@ -4,10 +4,8 @@ import botobo.core.domain.RepositoryTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
