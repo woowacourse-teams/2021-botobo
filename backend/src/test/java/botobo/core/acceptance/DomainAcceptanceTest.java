ca1811df7e0a0235f39d690da625f24a859ba686
seovalue <seominjeong.dev@gmail.com>
2021-10-25 18:32:10 +0900

    etc: 코드 정리

--- backend/src/test/java/botobo/core/acceptance/DomainAcceptanceTest.java
+++ backend/src/test/java/botobo/core/acceptance/DomainAcceptanceTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
